0f5a88e046d72f2760d13c35394f33fe3700f362
RogerMasc <rogersteam123@gmail.com>
2022-06-03 09:18:38 +0200

    DELETE

--- biolineappfinal/android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package bioline.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- biolineappfinal/lib/index.dart
+++
@@ -1,6 +0,0 @@
-// Export pages
-export 'login/login_widget.dart' show LoginWidget;
-export 'phone_sign_in/phone_sign_in_widget.dart' show PhoneSignInWidget;
-export 'create_account/create_account_widget.dart' show CreateAccountWidget;
-export 'change_password/change_password_widget.dart' show ChangePasswordWidget;
-export 'main_page/main_page_widget.dart' show MainPageWidget;
